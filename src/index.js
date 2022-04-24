ee698f24636dd949245011e4112cc0b391d2f0e1
Brandon Doornbos <b.doornbos@protonmail.com>
2022-04-24 13:21:04 +0200

    Only export user-facing stuff to the user
    Users should not be able te mess with the buffers for instance.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,7 @@ export * as Colors from './colors.json';
 export * from './common.js';
 export * from './renderer.js';
 export * from './camera.js';
-export * from './shader_source.js';
-export * from './shader.js';
-export * from './vertex_array.js';
-export * from './buffer.js';
-export * from './font.js';
 export * from './texture.js';
 export * from './utility.js';
 export * from './math.js';
 export * from 'gl-matrix';
-
-/**
- * When the library is built, this object encompasses all methods and members.
- * @namespace HB
- */
